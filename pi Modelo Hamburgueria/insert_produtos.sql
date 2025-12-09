d503498caba92c002e364f863fe44d27572a2334
JOAO VITOR <149392154+joao2740@users.noreply.github.com>
2025-12-09 15:07:37 -0300

    Adicoes

--- pi Modelo Hamburgueria/insert_produtos.sql
+++ pi Modelo Hamburgueria/insert_produtos.sql
@@ -1,12 +1,12 @@
 -- Inserir produtos de exemplo no cardápio
 INSERT INTO Produtos (Nome, Descricao, Quantidade, Preco_Unitario, Categoria, imagem, disponivel, destaque) VALUES
-('Classic Burger', 'Hambúrguer 180g, queijo, alface, tomate e molho especial', 50, 25.90, 'burgers', '', TRUE, TRUE),
-('X-Bacon', 'Hambúrguer 180g, queijo, bacon crocante, alface e tomate', 40, 28.90, 'burgers', '', TRUE, FALSE),
-('X-Tudo', 'Hambúrguer 180g, queijo, bacon, ovo, presunto, alface, tomate', 30, 32.90, 'burgers', '', TRUE, TRUE),
-('X-Salada', 'Hambúrguer 180g, queijo, alface, tomate, cebola e molho especial', 45, 24.90, 'burgers', '', TRUE, FALSE),
-('Smash Burger', 'Dois hambúrgueres smash 100g cada, queijo cheddar e molho especial', 35, 29.90, 'burgers', '', TRUE, TRUE),
+('Classic Burger', 'Hambúrguer 180g, queijo, alface, tomate e molho especial', 50, 25.90, 'burgers', 'https://thespiceway.com/cdn/shop/files/Signature_Savory_Classic_Burger.jpg?v=1712161801', TRUE, TRUE),
+('X-Bacon', 'Hambúrguer 180g, queijo, bacon crocante, alface e tomate', 40, 28.90, 'burgers', 'https://alloydeliveryimages.s3.sa-east-1.amazonaws.com/item_images/7290/64ceb8f870980813qe.webp', TRUE, FALSE),
+('X-Tudo', 'Hambúrguer 180g, queijo, bacon, ovo, presunto, alface, tomate', 30, 32.90, 'burgers', 'https://conteudo.imguol.com.br/c/entretenimento/17/2023/05/24/x-tudo-brasileiro-tem-variedade-de-ingredientes-de-acordo-com-preferencias-regionais-aqui-versao-com-carne-bovina-tomato-salsicha-presunto-bacon-e-queijo-no-pao-1684938396547_v2_900x506.jpg', TRUE, TRUE),
+('X-Salada', 'Hambúrguer 180g, queijo, alface, tomate, cebola e molho especial', 45, 24.90, 'burgers', 'https://alloydeliveryimages.s3.sa-east-1.amazonaws.com/item_images/7290/64ceb8f870980813qe.webp', TRUE, FALSE),
+('Smash Burger', 'Dois hambúrgueres smash 100g cada, queijo cheddar e molho especial', 35, 29.90, 'burgers', 'https://ogimg.infoglobo.com.br/rioshow/25033674-ea4-3ac/FT1086A/smash-burger.jpg', TRUE, TRUE),
 
-('Coca-Cola 350ml', 'Refrigerante Coca-Cola lata 350ml gelada', 100, 6.00, 'bebida', '', TRUE, FALSE),
+('Coca-Cola 350ml', 'Refrigerante Coca-Cola lata 350ml gelada', 100, 6.00, 'bebida', 'https://thumbs.dreamstime.com/b/coca-cola-em-alum%C3%ADnio-pode-fundo-preto-devon-reino-unido-de-outubro-162763145.jpg?w=576', TRUE, FALSE),
 ('Coca-Cola 2L', 'Refrigerante Coca-Cola garrafa 2 litros', 50, 12.00, 'bebida', '', TRUE, FALSE),
 ('Guaraná Antarctica 350ml', 'Refrigerante Guaraná lata 350ml gelada', 80, 5.50, 'bebida', '', TRUE, FALSE),
 ('Suco Natural', 'Suco natural de frutas 300ml (laranja, limão ou maracujá)', 40, 8.00, 'bebida', '', TRUE, FALSE),
